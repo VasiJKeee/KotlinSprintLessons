2d34e81f25114592373b5d83066f922e1d19dd9f
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-18 17:38:01 +0300

    Задача 4 к Уроку 2

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main(){
+
+   var crystal= 7
+   var iron = 11
+   var buff = 0.2
+
+
+   var bonusCrystal = (crystal * buff).toInt()
+   var bonusIron = (iron * buff).toInt()
+println("bonusCrystal $bonusCrystal bonusIron $bonusIron")
+}
+
