8e64cf53226255e1ebd9df6585a9dab1e6b6c149
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-18 13:43:48 +0300

    1 задача 2 урок

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,7 +1,13 @@
 package org.example.lesson_1
 
 
+var numberOforders = 64
+var textThanking = "Спасибочки епрст"
+var numberOfEmployees = 2000
+
 fun main(){
-println("тестовое решение")
-    println("какие то доработки")
+    println("Количество заказов: $numberOforders,  $textThanking")
+//    println(numberOfEmployees)
+    numberOfEmployees = numberOfEmployees -1
+    println(numberOfEmployees)
 }
