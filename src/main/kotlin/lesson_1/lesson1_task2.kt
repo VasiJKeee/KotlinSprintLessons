9e80896727ceb0136454c9a4e82b482416e299bc
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-18 13:47:45 +0300

    1 задача 2 урок правленная

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_1
 
 
-var numberOforders = 64
-var textThanking = "Спасибочки епрст"
-var numberOfEmployees = 2000
+
 
 fun main(){
+    var numberOforders = 64
+    var textThanking = "Спасибочки епрст"
+    var numberOfEmployees = 2000
     println("Количество заказов: $numberOforders,  $textThanking")
 //    println(numberOfEmployees)
     numberOfEmployees = numberOfEmployees -1
