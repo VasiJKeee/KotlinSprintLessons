c392982c9f222aa2a7713d56e0e98aea5520ea2c
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-19 12:54:26 +0300

    Задача 4 к Уроку 3

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var move = "$from-$to"
+    println(move)
+    from = "D2"
+    to = "D3"
+    move = "$from-$to"
+    println(move)
+}
