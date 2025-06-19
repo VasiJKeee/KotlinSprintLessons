00c5d8f1a9093a518e23e026e3694669de4f0110
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-19 12:41:51 +0300

    Задача 2 к Уроку 3

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main(): Unit {
+    val name = "Татьяна"
+    val lastName = "Сергеевна"
+    var surname = "Андреева"
+    var age = 20
+    println("$surname $name $lastName, $age")
+
+    surname = "Сидорова"
+    age = 22
+    println("$surname $name $lastName, $age")
+}
