284edb7a63085eaafed6cb7e2d6d2062548a0b4c
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-19 12:33:31 +0300

    Задача 1 к Уроку 3

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_3
+
+fun main (){
+    var textMorning = "доброе утречко епрст, "
+    var textEvening = "добрый вечер епрст, "
+    var name = "Адрей"
+    println("$textMorning$name")
+    println("$textEvening$name")
+}
