ec18f3040ae33cd78988fc7251d80abef9c11e73
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-20 10:35:35 +0300

    изменил переменную процентов

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -2,13 +2,11 @@ package org.example.lesson_2
 
 fun main(){
 
-   var crystal= 7
+   var crystal = 7
    var iron = 11
-   var buff = 0.2
-
-
-   var bonusCrystal = (crystal * buff).toInt()
-   var bonusIron = (iron * buff).toInt()
+   var buff = 20
+   var bonusCrystal = (crystal * buff)/100.toInt()
+   var bonusIron = (iron * buff)/100.toInt()
 println("bonusCrystal $bonusCrystal bonusIron $bonusIron")
 }
 
