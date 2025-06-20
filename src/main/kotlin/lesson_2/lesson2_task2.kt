e514fa9a0bb53b65bc66a178dea0632197cae0c9
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-20 10:21:51 +0300

    назв. переменной с мал. буквы + отступ + название пр

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_2
 
 fun main(){
-   var allParty = 50
+    var allParty = 50
     var allPartyMoney = 30000
-    var CountJun = 30
+    var countJun = 30
     var CountJunMoney = 20000
 
     var allPartyMoneyInM = allParty * allPartyMoney
-    var allPartyMoneyWithcJunInM = CountJun * CountJunMoney + allPartyMoneyInM
-    var averageSalary = allPartyMoneyWithcJunInM / (allParty + CountJun)
+    var allPartyMoneyWithcJunInM = countJun * CountJunMoney + allPartyMoneyInM
+    var averageSalary = allPartyMoneyWithcJunInM / (allParty + countJun)
     println(averageSalary)
 }
+
 
