6ea7fdb3e621ccfcff3b805601c64cce5da3d2e1
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-20 10:29:15 +0300

    все сделал, не понял только про магическое число

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -3,13 +3,15 @@ package org.example.lesson_2
 fun main(){
 
    val departureHour = 9
-      val departureMinute = 39
-      val travelTimeMinutes = 457
+   val departureMinute = 39
+   val travelTimeMinutes = 457
+   val minInHour = 60
+   val hoursInDay = 24
 
-      val totalDepartureMinutes = departureHour * 60 + departureMinute
+      val totalDepartureMinutes = departureHour * minInHour + departureMinute
       val totalArrivalMinutes = totalDepartureMinutes + travelTimeMinutes
-      val arrivalHour = (totalArrivalMinutes / 60) % 24
-      val arrivalMinute = totalArrivalMinutes % 60
+      val arrivalHour = (totalArrivalMinutes / minInHour) % hoursInDay
+      val arrivalMinute = totalArrivalMinutes % minInHour
 
    println("Время прибытия поезда: $arrivalHour:$arrivalMinute")
 }
