b131e0c8c5cea9494f059877c7365d8e30713127
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-20 10:17:05 +0300

    исправил .sum

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,11 +2,7 @@ package org.example.lesson_2
 
 fun main(){
     var ratings = intArrayOf(3, 4, 3, 5)
-    var b   = 0.00
-    for(i in ratings) {
-        b = b + i
-    }
-    var averageScore:Double = b / ratings.size
+    var averageScore = ratings.sum().toDouble() / ratings.size
 
-println(averageScore)
+    println(averageScore)
 }
