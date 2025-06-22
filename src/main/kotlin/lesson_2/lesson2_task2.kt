02e62c50851541a369a78bc9cabcb8cc2a193aea
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-22 10:13:38 +0300

    исправил все переменые в мал. буквы

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -4,10 +4,10 @@ fun main(){
     var allParty = 50
     var allPartyMoney = 30000
     var countJun = 30
-    var CountJunMoney = 20000
+    var countJunMoney = 20000
 
     var allPartyMoneyInM = allParty * allPartyMoney
-    var allPartyMoneyWithcJunInM = countJun * CountJunMoney + allPartyMoneyInM
+    var allPartyMoneyWithcJunInM = countJun * countJunMoney + allPartyMoneyInM
     var averageSalary = allPartyMoneyWithcJunInM / (allParty + countJun)
     println(averageSalary)
 }
