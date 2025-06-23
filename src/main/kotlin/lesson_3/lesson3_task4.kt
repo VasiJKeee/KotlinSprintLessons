1eaa49cdb571daebcd2c89cd4a9dc7294572dec6
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-23 11:24:05 +0300

    добавил переменную для значения хода

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -4,13 +4,15 @@ fun main() {
 
     var from = "E2"
     var to = "E4"
-    var move = "$from-$to"
+    var moveNumber = 1
+    var move = "$from-$to;$moveNumber"
 
     println(move)
 
     from = "D2"
     to = "D3"
-    move = "$from-$to"
+    moveNumber += moveNumber
+    move = "$from-$to;$moveNumber"
 
     println(move)
 
