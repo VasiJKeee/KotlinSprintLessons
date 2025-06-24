f7478f234dbf97422c25bacfae133e125510378c
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-24 17:26:01 +0300

    вынес в отдельную переменную check

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -2,18 +2,17 @@ package org.example.lesson_4
 
 fun main() {
 
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+    var minWeight = 35
+    var maxWeight = 100
+    var maxVolume = 100
 
     var weight = 20
     var volume = 80
-
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " + (weight > minWeight && weight <= maxWeight && volume < maxVolume))
+    var check = weight > minWeight && weight <= maxWeight && volume < maxVolume
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " + check)
 
     weight = 50
     volume = 100
-
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " + (weight > minWeight && weight <= maxWeight && volume < maxVolume))
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " + check)
 
 }
