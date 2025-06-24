fcd6eed258a84fccdd0d4005553d2cf6d8ee79e9
VasiJKeee <126481096+VasiJKeee@users.noreply.github.com>
2025-06-24 17:20:09 +0300

    убрал магические числа

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -2,11 +2,12 @@ package org.example.lesson_4
 
 fun main() {
 
-    val weatherToday: Boolean = true
-    val nowTentOpen: Boolean = true
-    val airDry: Int = 20
-    val season: String = "зима"
-
-    val total = weatherToday && nowTentOpen && airDry == 20 && season != "зима"
+    val isSunny: Boolean = true
+    val isTentOpen: Boolean = true
+    var airDry: Int = 20
+    var season: String = "зима"
+    val seasonNow = "зима"
+    val airDryNow = 20
+    val total = isSunny && isTentOpen && airDry == airDryNow && season != seasonNow
     println("Благоприятные ли условия сейчас для роста бобовых? $total")
 }
